ae75d30853ec3b9b967c831a48294e107e814c17
SidhartP01 <sidharthpillayil@gmail.com>
2025-03-20 12:32:13 +0530

    comparisons tasks completed

---
+++ 2_Javascript_fundamentals/2.9_Comparisons/comparisons.js
@@ -0,0 +1,16 @@
+// 5 > 4
+// "apple" > "pineapple"
+// "2" > "12"
+// undefined == null
+// undefined === null
+// null == "\n0\n"
+// null === +"\n0\n"
+
+console.log(5>4); // true
+console.log("apple" > "pineapple"); // false 
+console.log("aaaa" > "aa");
+console.log("2" > "12"); // true
+console.log(undefined == null); //true
+console.log(undefined === null); // false
+console.log(null == "\n0\n"); //false
+console.log(null === +"\n0\n"); //false
