b3f6f55d633ad7229c97539842782efcc2850407
SidhartP01 <sidharthpillayil@gmail.com>
2025-03-20 10:47:10 +0530

    datatypes task completed

---
+++ 2_Javascript_fundamentals/2.4_Variables/uppercase.js
@@ -0,0 +1,4 @@
+const BIRTHDAY = '18.04.1982';
+const AGE = someCode(BIRTHDAY);  // to understand const we declare them in caps
+
+console.log(AGE);
---
+++ 2_Javascript_fundamentals/2.5_Datatypes/datatypes.js
@@ -0,0 +1,7 @@
+let n = "Sidharth";
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${n}` ); // hello Sidharth
