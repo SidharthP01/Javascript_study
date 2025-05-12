0820ce499f23807ff378ce2576836f6d4acb82c4
SidhartP01 <sidharthpillayil@gmail.com>
2025-05-12 10:14:28 +0530

    css animations task completed

--- demo.js
+++
@@ -1,7 +0,0 @@
-console.log("A");
-let pro = new Promise((resolve,reject) => reject("Error"));
-
-pro.then((value) =>{
-    console.log(value);
-}).catch((err) => console.log(err));
-console.log("C");
